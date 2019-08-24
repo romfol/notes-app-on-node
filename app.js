8f34184d5bac99e1b967b50de335789aa48b1124
romfol <romanfoltyn1@gmail.com>
2019-08-24 22:36:05 +0300

    yargs commands

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const notes = require("./notes")
 
 
 
-console.log(yargs.argv);
-console.log(process.argv);
+//  console.log(yargs);
+// console.log(process.argv);
 // console.log(validator.isEmail('ddd@gmail.com'));
+yargs.command({
+  command: 'add',
+  describe: 'listing',
+  handler: function() {
+    console.log('11wsqws')
+  }
+})
+
+console.log(yargs.argv);
