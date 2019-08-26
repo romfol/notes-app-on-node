35d0ff0796e2f34c6ef41c794a05017c2adbe557
romfol <romanfoltyn1@gmail.com>
2019-08-26 19:56:25 +0300

    removed debugger

--- notes.js
+++ notes.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find(note => title===note.title)
-debugger
+
   if(!duplicateNote) {
      notes.push({
       title,
